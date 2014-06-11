a7c023575054768f2e72a56ab4a4c3917be3005c
Adam Magaluk <AdamMagaluK@gmail.com>
2014-06-10 21:26:40 -0400

    Added more tests to url.parse url.format and url.resolve

--- test/suite/url.js
+++ test/suite/url.js
@@ -20,13 +20,50 @@ console.log(t + '..' + tmax);
 ok(process.versions.colony, 'running in colony')
 
 var url = require('url');
+
+// test that url returns an object
 ok(url.parse('http://tools.ietf.org/html/draft-ietf-6man-text-addr-representation-04#section-6'), 'url parses');
 
+
+// test parsing path url
+var expectedPathUrl = { protocol: null,
+  slashes: null,
+  auth: null,
+  host: null,
+  port: null,
+  hostname: null,
+  hash: null,
+  search: null,
+  query: null,
+  pathname: '/one/two/three',
+  path: '/one/two/three',
+  href: '/one/two/three' };
+
+var actualPathUrl = url.parse('/one/two/three');
+Object.keys(expectedPathUrl).forEach(function(k){
+  equal(actualPathUrl[k], expectedPathUrl[k], k + ' should match expected path url')
+});
+
+
+
+// parse url
 var actual = url.parse('ws://user:pass@somedomain.com:1234/events?q=123#hash1');
 
+// test all properties on expected url obj vs pared object
 Object.keys(expected).forEach(function(k){
-  
   equal(actual[k], expected[k], k + ' should matched expected ')
 });
 
+
+// test url.parse for .hostname
 ok(url.parse('http://api.openweathermap.org/data/2.5/weather?id=5327684&units=imperial').hostname == 'api.openweathermap.org', 'hostname match');
+
+
+// test url.format
+equal(url.format(expected), 'ws://user:pass@somedomain.com:1234/events?q=123#hash1', 'url.format check');
+
+// Test url.resolve
+equal(url.resolve('/one/two/three', 'four'), '/one/two/four', 'url.resolve 1');
+equal(url.resolve('http://example.com/', '/one'), 'http://example.com/one', 'url.resolve 2');
+equal(url.resolve('http://example.com/one', '/two'), 'http://example.com/two', 'url.resolve 3');
+
